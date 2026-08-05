6f0dd0bab6bc9ee8e4d684bd8847fd84fce1cbe3
langeneggerotto-creator <langenegger.otto@gmail.com>
2026-08-05 13:05:18 -0600

    feat(dmf): expose capability atlas API

---
+++ dreammusicforge/capability_atlas/__init__.py
@@ -0,0 +1,15 @@
+from .atlas import (
+    RendererRecommendation,
+    ShotRequirement,
+    evaluate_renderer,
+    rank_renderers,
+    validate_profile,
+)
+
+__all__ = [
+    "RendererRecommendation",
+    "ShotRequirement",
+    "evaluate_renderer",
+    "rank_renderers",
+    "validate_profile",
+]
