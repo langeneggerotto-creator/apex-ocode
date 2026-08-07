332e2dac6df81765048dbbb95152df363a8a8637
langeneggerotto-creator <langenegger.otto@gmail.com>
2026-08-07 10:02:37 -0600

    feat(dmf): add Kling provider package init

---
+++ dreammusicforge/providers/kling/__init__.py
@@ -0,0 +1,4 @@
+from .compiler import compile_kling_package
+from .models import KlingExecutionPackage, KlingReference, KlingMode
+
+__all__ = ["compile_kling_package", "KlingExecutionPackage", "KlingReference", "KlingMode"]
